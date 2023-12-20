da22c5f459e6e76686aced73dfdea898e1141df9
paysion <s1543795068@163.com>
2023-12-21 00:05:25 +0800

    p695 服务端新增获取在线用户列表

--- src/main/java/com/zoc/qqserver/service/ManageClientThreads.java
+++ src/main/java/com/zoc/qqserver/service/ManageClientThreads.java
@@ -1,6 +1,7 @@
 package com.zoc.qqserver.service;
 
 import java.util.HashMap;
+import java.util.Iterator;
 
 /**
  * 该类用于管理和客户端通信的线程
@@ -13,5 +14,15 @@ public class ManageClientThreads {
         return hm;
     }
 
-
+    // 编写方法返回在线用户列表
+    public static String getOnlineUser() {
+        // 遍历hashmap的key获取用户列表,使用迭代器？
+        Iterator<String> iterator = hm.keySet().iterator();
+        String onlineUserList = "";
+        // 这里涉及到迭代器的使用？
+        while (iterator.hasNext()) {
+            onlineUserList += iterator.next().toString() + " ";
+        }
+        return onlineUserList;
+    }
 }
--- src/main/java/com/zoc/qqserver/service/ServerConnectClientThread.java
+++ src/main/java/com/zoc/qqserver/service/ServerConnectClientThread.java
@@ -1,6 +1,7 @@
 package com.zoc.qqserver.service;
 
 import com.zoc.qqcommon.Message;
+import com.zoc.qqcommon.MessageType;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -26,6 +27,11 @@ public class ServerConnectClientThread extends Thread{
                 ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
                 // 这里的message后边会使用到，暂时没发现这个message有什么用，暂且放一边
                 Message message = (Message) ois.readObject();
+                if (message.getMesType().equals(MessageType.MESSAGE_GET_ONLINE_FRIEND)) {
+                    // 客户端要在线用户列表
+                    System.out.println(message.getSender() + " 要在线用户列表");
+                    ManageClientThreads.getOnlineUser();
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
